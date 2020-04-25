27b640bf630c92df613cddf9f78e0e8c8e64bd4f
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-04-25 16:02:28 -0400

    Ex 10.20.b

--- ch10/exo_10.20.R
+++ ch10/exo_10.20.R
@@ -1,8 +1,12 @@
-# rm(list = ls())
-load("~/Documents/Umontreal/STT1700/git/ch10/exo_10.30/ex10-20twister.rda")
+#rm(list = ls())
+load("~/Documents/Umontreal/STT1700/GitHub/ch10/ex10-20twister.rda")
 tornades <- `EX10-20TWISTER`
 rm(`EX10-20TWISTER`)
 plot(tornades$Year, tornades$Tornadoes)
 reg<-lm(Tornadoes ~ Year, data = tornades)
 abline(reg)
-summary(reg)
+
+#http://r-statistics.co/Linear-Regression.html
+beta.estimate <- summary(reg)$coefficients["Year", "Estimate"]
+std.error <-summary(reg)$coefficients["Year", "Std. Error"]
+beta.estimate + c(-1,1) * qt(0.975,60) * std.error
