0db0a1f7cd4d81636a5975e9d770dd7751b2f293
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-04-25 16:11:10 -0400

    predict

--- ch10/exo_10.20.R
+++ ch10/exo_10.20.R
@@ -1,12 +1,16 @@
 #rm(list = ls())
 load("~/Documents/Umontreal/STT1700/GitHub/ch10/ex10-20twister.rda")
-tornades <- `EX10-20TWISTER`
+data.frame <- `EX10-20TWISTER`
 rm(`EX10-20TWISTER`)
-plot(tornades$Year, tornades$Tornadoes)
-reg<-lm(Tornadoes ~ Year, data = tornades)
-abline(reg)
+plot(data.frame$Year, data.frame$Tornadoes)
+reg.lin <- lm(Tornadoes ~ Year, data = data.frame)
+abline(reg.lin)
 
 #http://r-statistics.co/Linear-Regression.html
-beta.estimate <- summary(reg)$coefficients["Year", "Estimate"]
-std.error <-summary(reg)$coefficients["Year", "Std. Error"]
+beta.estimate <- summary(reg.lin)$coefficients["Year", "Estimate"]
+std.error <-summary(reg.lin)$coefficients["Year", "Std. Error"]
 beta.estimate + c(-1,1) * qt(0.975,60) * std.error
+
+predict(reg.lin, data.frame(Year = c(2015)))
+
+predict(reg.lin, data.frame(Year = c(2015)), interval = "predict")
