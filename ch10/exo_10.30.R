00c3abfda30e0c49a1b75875c02485f11f405d02
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-04-26 05:23:21 -0400

    summary

--- ch10/exo_10.30.R
+++ ch10/exo_10.30.R
@@ -1,18 +1,19 @@
-#rm(list = ls())
+rm(list = ls())
+cat("\014")
 load("~/Documents/Umontreal/STT1700/GitHub/ch10/ex10-30gradeup.rda")
 df <- `EX10-30GRADEUP`
 rm(`EX10-30GRADEUP`)
 plot(df$Year, df$GPA)
 reg.lin <- lm(GPA ~ Year, data = df)
 abline(reg.lin)
-summary(reg.lin)
-r<-cor(df$Year,df$GPA)
+r <- cor(df$Year,df$GPA)
 sx <- sd(df$Year)
 sy <- sd(df$GPA)
 b_hat <- r * sy/sx
 x_bar <- mean(df$Year)
 y_bar <- mean(df$GPA)
 a_hat <- y_bar - b_hat * x_bar
+summary(reg.lin)
 -1.859e+01
 sprintf("%.100f",a_hat)
 1.076e-02
