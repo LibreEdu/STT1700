04ac14f375b71fb7de5f0fc839c3d23539704f32
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-04-25 22:03:49 -0400

    sprintf

--- ch10/exo_10.30.R
+++ ch10/exo_10.30.R
@@ -9,11 +9,22 @@ summary(reg.lin)
 r<-cor(df$Year,df$GPA)
 sx <- sd(df$Year)
 sy <- sd(df$GPA)
-b <- r * sy/sx
-xb <- mean(df$Year)
-yb <- mean(df$GPA)
-a <- yb - b * xb
+b_hat <- r * sy/sx
+x_bar <- mean(df$Year)
+y_bar <- mean(df$GPA)
+a_hat <- y_bar - b_hat * x_bar
 -1.859e+01
-sprintf("%.100f",a)
+sprintf("%.100f",a_hat)
 1.076e-02
-sprintf("%.100f",b)
+sprintf("%.100f",b_hat)
+
+# intervalle de confiance de niveau 95 % pour la pente 
+beta.estimate <- summary(reg.lin)$coefficients["Year", "Estimate"]
+std.error <-summary(reg.lin)$coefficients["Year", "Std. Error"]
+p <- 0.975
+df <- 2
+IC_min <- beta.estimate - qt(p,df) * std.error
+IC_max <- beta.estimate + qt(p,df) * std.error
+sprintf("%.100f",IC_min)
+sprintf("%.100f",IC_max)
+beta.estimate + c(-1,1) * qt(p,df) * std.error
