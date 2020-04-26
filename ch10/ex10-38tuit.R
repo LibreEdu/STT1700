811044d3823588b9a4cc6493f1081f06df8dec34
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-04-26 18:02:08 -0400

    subset=(CalifSchool==0)

--- ch10/ex10-38tuit.R
+++ ch10/ex10-38tuit.R
@@ -1,14 +1,13 @@
 rm(list = ls())
-cat("\014")
 graphics.off()
+cat("\014")
 
-load("~/Documents/Umontreal/STT1700/GitHub/ch10/ex10-38tuit.rda")
-df <- `EX10-38TUIT`
+df <- get(load("~/Documents/Umontreal/STT1700/GitHub/ch10/ex10-38tuit.rda"))
 rm(`EX10-38TUIT`)
 
 plot(df$Y2008, df$Y2014, xlab = "Frais de scolarité en 2008", ylab = "Frais de scolarité en 2014")
 lm <- lm(Y2014 ~ Y2008, data = df)
-abline(reg.lin)
+abline(lm)
 
 r <- cor(df$Y2008,df$Y2014)
 sx <- sd(df$Y2008)
@@ -21,4 +20,22 @@ summary(lm)
 sprintf("%.100f",a_hat)
 sprintf("%.100f",b_hat)
 
-plot(lm)
+df <- cbind(df, lm$residuals)
+colnames(df)[6] <- "Residu"
+plot(df$Y2008, df$Residu, xlab = "Frais de scolarité en 2008", ylab = "Résidus")
+lm2 <- lm(Residu ~ Y2008, data = df)
+abline(lm2)
+
+qqnorm(df$Y2014)
+qqline(df$Y2014)
+
+qqnorm(df$Residu)
+qqline(df$Residu)
+
+qqnorm(lm$residuals)
+qqline(lm$residuals)
+
+lm3 <- lm(Y2014 ~ Y2008, data = df, subset=(CalifSchool==0))
+lm3$coefficients
+qqnorm(lm3$residuals)
+qqline(lm3$residuals)
