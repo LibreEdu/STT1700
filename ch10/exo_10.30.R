220f625733968a7c68e833084fc48961d2e0bbb0
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-04-25 21:30:08 -0400

    10.30.b

---
+++ ch10/exo_10.30.R
@@ -0,0 +1,19 @@
+#rm(list = ls())
+load("/Users/pachot/Documents/Umontreal/STT1700/GitHub/ch10/ex10-30gradeup.rda")
+df <- `EX10-30GRADEUP`
+rm(`EX10-30GRADEUP`)
+plot(df$Year, df$GPA)
+reg.lin <- lm(GPA ~ Year, data = df)
+abline(reg.lin)
+summary(reg.lin)
+r<-cor(df$Year,df$GPA)
+sx <- sd(df$Year)
+sy <- sd(df$GPA)
+b <- r * sy/sx
+xb <- mean(df$Year)
+yb <- mean(df$GPA)
+a <- yb - b * xb
+-1.859e+01
+sprintf("%.100f",a)
+1.076e-02
+sprintf("%.100f",b)
