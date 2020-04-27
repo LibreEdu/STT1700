0344d0c515fe638f1193afd95a5ead7afe9ae0db
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-04-27 11:00:46 -0400

    Taille échantillonale

--- ch10/ch9-chi.R
+++ ch10/ch9-chi.R
@@ -4,15 +4,33 @@ sprintf("%.100f",qchisq(.8,1))
 1-pchisq(4.1626,4)
 1-pchisq(15.64,5)
 1-pchisq(97.439,5)
-rbinom(1, 800, 0.3)
+rbinom(2, 800, 0.3)
 
-p <- .3
-n <- 800
+cat("\014")
+p <- .43
+n <- 1430
 var <- p*(1-p)/n
 s <- sqrt(var)
-alpha = 5/100
+
+# alpha = 5/100 => IC à 95 %
+alpha <- 5/100
 z <- qnorm(1-alpha/2)
-ICmin <- p - z*s
-ICmax <- p + z*s
+# alpha = 5/100 => z =1.96
+sprintf("%.100f",z)
+
+marge.erreur <- z*s
+sprintf("%.100f",marge.erreur)
+
+ICmin <- p - marge.erreur
+ICmax <- p + marge.erreur
 sprintf("%.100f",ICmin)
 sprintf("%.100f",ICmax)
+
+# Taille échantillonale : arrondir au plafond
+p <- .2
+alpha <- 5/100
+z <- qnorm(1-alpha/2)
+marge.erreur <- 0.07
+n <- p * (1-p) * z^2 / marge.erreur^2
+n
+ceiling(n)
